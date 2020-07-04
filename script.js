5718966f4bc7c8999372eba8bfb92251c9afd5c2
Zack Shone <zackariahshone@gmail.com>
2020-07-04 13:06:36 -0700

    added logic to change color depending on hour

--- script.js
+++ script.js
@@ -1,11 +1,106 @@
 $(document).ready(function () {
-    
-function getCurrentdate(){
     let date = new Date().toDateString();
+    let time = new Date().getHours();
+    let row = document.querySelectorAll('.row');
+    //let time = 11;
+    function getCurrentdate() {
 
-    document.getElementById('timeStamp').textContent = date;
-}
+        document.getElementById('timeStamp').textContent = date;
+        document.getElementById('currentTime').textContent = time;
 
-getCurrentdate();
+    }
+    getCurrentdate();
+    
+    
+
+    //time management system...
+    if (time > 8) {
+        $('#8').addClass('past');
+    }
+    if (time === 8) {
+        $('#8').addClass('present');
+    }
+    if (time < 8) {
+        $('#8').addClass('future');
+    }
+
+    if (time > 9) {
+        $('#9').addClass('past');
+    }
+    if (time === 9) {
+        $('#9').addClass('present');
+    }
+    if (time < 9) {
+        $('#9').addClass('future');
+    }
+
+    if (time > 10) {
+        $('#10').addClass('past');
+    }
+    if (time === 10) {
+        $('#10').addClass('present');
+    }
+    if (time < 10) {
+        $('#10').addClass('future');
+    }
+
+    if (time > 11) {
+        $('#11').addClass('past');
+    }
+    if (time === 11) {
+        $('#11').addClass('present');
+    }
+    if (time < 11) {
+        $('#11').addClass('future');
+    }
+
+    if (time > 12) {
+        $('#12').addClass('past');
+    }
+    if (time === 12) {
+        $('#12').addClass('present');
+    }
+    if (time < 12) {
+        $('#12').addClass('future');
+    }
+
+    if (time > 13) {
+        $('#1').addClass('past');
+    }
+    if (time === 13) {
+        $('#1').addClass('present');
+    }
+    if (time < 13) {
+        $('#1').addClass('future');
+    }
+    if (time > 14) {
+        $('#2').addClass('past');
+    }
+    if (time === 14) {
+        $('#2').addClass('present');
+    }
+    if (time < 14) {
+        $('#2').addClass('future');
+    }
+    if (time > 15) {
+        $('#3').addClass('past');
+    }
+    if (time === 15) {
+        $('#3').addClass('present');
+    }
+    if (time < 15) {
+        $('#3').addClass('future');
+    }
+    if (time > 16) {
+        $('#4').addClass('past');
+    }
+    if (time === 16) {
+        $('#4').addClass('present');
+    }
+    if (time < 16) {
+        $('#4').addClass('future');
+    }
+
+    $()
 
 });
