2a55eca4fe179fa4263348fc15a86f1db0dda8de
Zack Shone <zackariahshone@gmail.com>
2020-07-06 21:12:54 -0700

    took out increment

--- script.js
+++ script.js
@@ -7,18 +7,11 @@ $(document).ready(function () {
    
    // let time = 8;
     function getCurrentdate() {
-
         document.getElementById('timeStamp').textContent = date;
         document.getElementById('currentTime').textContent = time;
-
     }
     getCurrentdate();
-
-   
-
     //time management system...
-    
-    
     function setColor() {
 
         for(let i = 8; i <= 16; i++ ){
@@ -33,11 +26,11 @@ $(document).ready(function () {
             }
         }
     }
-
     function hourly() {
         setInterval(setColor, 1000 * 3);
-        //setInterval(incrementTime, 1000*10);
+        
     }
+
     $('.saveBtn').click(function (e) {
         let index = e.target.value;
         console.log(index);
@@ -46,9 +39,5 @@ $(document).ready(function () {
         console.log(x);
         document.getElementById('note' + index).value = "";
     });
-     function incrementTime() {
-         time = time+1;
-         console.log(time); 
-     }
     hourly();
 });
